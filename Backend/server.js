eed6bf6e072f8bfef0df0d98f4fd9c7febe9b707
pb6969 <165070191+pb6969@users.noreply.github.com>
2025-06-24 13:37:18 +0530

    Add files via upload

---
+++ Backend/server.js
@@ -0,0 +1,11 @@
+require("dotenv").config()
+const app = require("./app")
+const mongoose = require("mongoose")
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log("Connected to MongoDB")
+  app.listen(5000, () => console.log("Backend running on http://localhost:5000"))
+}).catch(err => console.error("MongoDB connection error:", err))
